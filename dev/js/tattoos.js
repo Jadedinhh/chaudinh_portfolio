5aa035f9f60cff3b82415494ef5f0ac8ca63751b
Jadedinhh <mbd77@drexel.edu>
2022-03-16 10:51:39 -0400

    jv

---
+++ dev/js/tattoos.js
@@ -0,0 +1,51 @@
+export let tattoos = [
+    
+    {
+        caption: "Wu 吳 handpoked for Andrew",
+        images: ['./img/tattoos/wu.jpg']
+    },
+    {
+        caption: "Machine Queen of Hearts for Bonnie",
+        images: ['./img/tattoos/queen.jpg']
+    },
+    {
+        caption: "Machine pet dog for Thuy",
+        images: ['./img/tattoos/cun.jpg']
+    },
+    {
+        caption: "Martini Duck handpoked for Bryant",
+        images: ['./img/tattoos/duck.jpg']
+    },
+    {
+        caption: "Panpan handpoked for Wayne",
+        images: ['./img/tattoos/panda.jpg']
+    },
+    {
+        caption: "Butterfly handpoked for Linda",
+        images: ['./img/tattoos/leenda.jpg']
+    },
+    {
+        caption: "Gradient Moon handpoked for Kexin",
+        images: ['./img/tattoos/moon.jpg']
+    },
+    {
+        caption: "Chipmunk handpoked for Min",
+        images: ['./img/tattoos/chipmunk.jpg']
+    },
+    {
+        caption: "Cactus handpoked for David",
+        images: ['./img/tattoos/cactus.jpg']
+    },
+    {
+        caption: "8 Ball handpoked for David",
+        images: ['./img/tattoos/8ball.jpg']
+    },
+    {
+        caption: "Snake handpoked for Kyle",
+        images: ['./img/tattoos/snake.jpg']
+    },
+    {
+        caption: "Machine Script for Andrew",
+        images: ['./img/tattoos/dot.jpg']
+    }
+];
