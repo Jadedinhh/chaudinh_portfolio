522459e6ebed1f1e80c3118cf4275d030163b603
Jadedinhh <mbd77@drexel.edu>
2022-03-17 10:38:42 -0400

    change

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,42 +1,4 @@
 import { tattoos } from "./tattoos"
-// import { burgerTL } from "./burgerAnimation";
-// import {menuAnimation} from "./mobileMenu.js";
-// import {scrollPage} from "./pageScroll";
-// import { displayWindowSize } from "./mobileResizing"
-
-// var burgerButton = document.querySelector("#burger");
-// let canISeeMenu = false;
-
-// function openCloseMenu(){
-//     if(canISeeMenu === false){
-//         // can't see menu... play timeline of burger into X
-//         burgerTL.play();
-//         menuAnimation.play();
-//         canISeeMenu = true;
-//     }else{
-//         // can see menu.. play X back into burger
-//         burgerTL.reverse();
-//         menuAnimation.reverse();
-//         canISeeMenu = false;
-//     }
-// }
-
-// burgerButton.addEventListener("click", openCloseMenu);
-// let navButtons = document.querySelectorAll(".nav-btns");
-
-// for (const button of navButtons){
-//     button.addEventListener("click", checkScrolling);
-//     button.addEventListener("click", openCloseMenu);
-// }
-
-
-// function checkScrolling(e) {
-//     // check to see which button was clicked
-//     const indexValue = [].indexOf.call(navButtons, e.target)
-//     if (indexValue != -1) {
-//         scrollPage(indexValue);
-//     }
-// }
 
 window.addEventListener('load', function() {
 
@@ -65,8 +27,3 @@ window.addEventListener('load', function() {
     // });
 })
 
-// //listen to window resize
-// window.addEventListener("resize", displayWindowSize);
-
-// //check the window size on load
-// window.addEventListener('load', displayWindowSize);
