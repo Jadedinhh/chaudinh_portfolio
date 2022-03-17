5221765f0de4945bca3675ecfb0e52f522b04d24
Jadedinhh <mbd77@drexel.edu>
2022-03-17 12:23:47 -0400

    jv crash

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,8 @@
 import { tattoos } from "./tattoos";
-import { burgerTL } from "./burgerAnimation";
-import { menuAnimator } from "./mobileMenu";
-import { displayWindowSize } from "./mobileResizing";
-import {scrollPage } from "./pageScroll";
+// import { burgerTL } from "./burgerAnimation";
+// import { menuAnimator } from "./mobileMenu";
+// import { displayWindowSize } from "./mobileResizing";
+// import {scrollPage } from "./pageScroll";
 
 
 window.addEventListener('load', function() {
@@ -20,5 +20,48 @@ window.addEventListener('load', function() {
 
 });
 
+// var burgerButton = document.querySelector("#burger");
+
+// let burgerClicked = false;
+
+// function openCloseMenu(){
+//     if(burgerClicked === false){
+//       burgerTL.play();
+//       menuAnimator.play();
+//       burgerClicked = true;
+//   }else{
+//       burgerTL.reverse();
+//       menuAnimator.reverse();
+//       burgerClicked = false;
+//     } 
+//  }
+
+//  burgerButton.addEventListener("click", openCloseMenu);
+
+// let navButtons = document.querySelectorAll(".nav-btns");
+
+// for(const button of navButtons){
+//     button.addEventListener("click", checkScrolling);
+//     button.addEventListener("click", openCloseMenu);
+// }
 
 
+// function checkScrolling(e) {
+//     const indexValue = [].indexOf.call(navButtons, e.target)
+    
+//     if (indexValue != -1){
+//         scrollPage(indexValue -1);
+//     }
+// }
+
+// window.addEventListener("resize", function(){
+//     if(burgerClicked === true){
+//         console.log("fire");
+//         openCloseMenu();
+//     }
+// });
+
+
+// window.addEventListener("resize", displayWindowSize);
+
+// window.addEventListener('load', displayWindowSize);
