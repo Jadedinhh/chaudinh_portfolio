b5013d9d0169ac8ae93979d0a13f885f9f639287
Jadedinhh <mbd77@drexel.edu>
2022-03-17 10:37:16 -0400

    js

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,42 @@
 import { tattoos } from "./tattoos"
+// import { burgerTL } from "./burgerAnimation";
+// import {menuAnimation} from "./mobileMenu.js";
+// import {scrollPage} from "./pageScroll";
+// import { displayWindowSize } from "./mobileResizing"
+
+// var burgerButton = document.querySelector("#burger");
+// let canISeeMenu = false;
+
+// function openCloseMenu(){
+//     if(canISeeMenu === false){
+//         // can't see menu... play timeline of burger into X
+//         burgerTL.play();
+//         menuAnimation.play();
+//         canISeeMenu = true;
+//     }else{
+//         // can see menu.. play X back into burger
+//         burgerTL.reverse();
+//         menuAnimation.reverse();
+//         canISeeMenu = false;
+//     }
+// }
+
+// burgerButton.addEventListener("click", openCloseMenu);
+// let navButtons = document.querySelectorAll(".nav-btns");
+
+// for (const button of navButtons){
+//     button.addEventListener("click", checkScrolling);
+//     button.addEventListener("click", openCloseMenu);
+// }
+
+
+// function checkScrolling(e) {
+//     // check to see which button was clicked
+//     const indexValue = [].indexOf.call(navButtons, e.target)
+//     if (indexValue != -1) {
+//         scrollPage(indexValue);
+//     }
+// }
 
 window.addEventListener('load', function() {
 
@@ -9,8 +47,7 @@ window.addEventListener('load', function() {
     // iterate over the heroImgArray and populate the background images
     tattooImgArray.forEach((tattooImg, i) => {
 
-        // first pass in the array
-        // heroImg.style.backgroundImage = "url(./img/projects/turtles/turtle-1.jpg)";
+    
 
         tattooImg.style.backgroundImage = "url(" + tattoos[i].images[0] + ")";
 
@@ -27,3 +64,9 @@ window.addEventListener('load', function() {
     //     });
     // });
 })
+
+// //listen to window resize
+// window.addEventListener("resize", displayWindowSize);
+
+// //check the window size on load
+// window.addEventListener('load', displayWindowSize);
