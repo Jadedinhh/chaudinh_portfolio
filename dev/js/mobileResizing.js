b1d9537a60373ce9981ed34537cc7ee849dd88f5
Jadedinhh <mbd77@drexel.edu>
2022-03-18 10:26:43 -0400

    what

--- dev/js/mobileResizing.js
+++ dev/js/mobileResizing.js
@@ -3,13 +3,13 @@ import { gsap } from "gsap";
 export function displayWindowSize(){
 
      let menu = document.querySelector("#nav-container");
-     let menuHeight = menu.offsetHeight;
+     let menuWidth = menu.offsetWidth;
 
-     console.log(menuHeight);
+    //  console.log(menuHeight);
 
     if(document.documentElement.clientWidth <= 1024){
         console.log("hide");
-        gsap.set("#nav-container", {x:-menuHeight});
+        gsap.set("#nav-container", {x:-menuWidth});
     }else{
         console.log("un-hide");
         gsap.set("#nav-container",{x:0});
