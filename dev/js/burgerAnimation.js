b40afc99b7f190fddf321804a95a9a4ef0ab99a4
Jadedinhh <mbd77@drexel.edu>
2022-03-17 23:24:07 -0400

    burger

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -5,12 +5,12 @@ gsap.registerPlugin(DrawSVGPlugin);
 gsap.set(".burger-lines",{transformOrigin:"center"});
 
 const topTL = new gsap.timeline();
-topTL.to(".burger-lines:nth-child(1)",{duration:0.25, y:"+=.5"})
-.to(".burger-lines:nth-child(1)",{duration:0.25, rotation:45,fill:"#fff"});
+topTL.to(".burger-lines:nth-child(1)",{duration:0.25, y:"+=0.1"})
+.to(".burger-lines:nth-child(1)",{duration:0.8, rotation:225,fill:"#fff"});
 
 const bottomTL = new gsap.timeline();
-bottomTL.to(".burger-lines:nth-child(2)",{duration:0.25, y:"-=8.5"})
-.to(".burger-lines:nth-child(2)",{duration:0.25, rotation:-45,fill:"#fff"});
+bottomTL.to(".burger-lines:nth-child(2)",{duration:0.25, y:"-=8"})
+.to(".burger-lines:nth-child(2)",{duration:0.8, rotation:-225,fill:"#fff"});
 
 
 export const burgerTL = new gsap.timeline({paused:true});
